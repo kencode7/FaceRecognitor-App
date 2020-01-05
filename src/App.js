435f15e857cecd596b27a94c790bb80b5165ddb3
kenneth nwobi <kencode007@gmail.com>
2020-01-05 22:17:28 +0000

    holidays is over

--- src/App.js
+++ src/App.js
@@ -7,13 +7,31 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click')
+  }
+
   render() {
     return (
       <div className="App">
         <Navigation />
         <Logo />
         <Rank/>
-        <ImageLinkForm />
+        <ImageLinkForm 
+        onInputChange={this.onInputChange} 
+        onButtonSubmit={this.onButtonSubmit}
+        />
         {/*
         <FaceRecognition />*/}
       </div>
