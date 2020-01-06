624d3f903729718407fb80c85ddc2d41361843f2
kenneth nwobi <kencode007@gmail.com>
2020-01-06 14:42:23 +0000

    face detect model added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 
 import './App.css';
 
+const app = new Clarifai.App({
+  apiKey: '51132ea45b5a4c38bf4792cadcfc555e'
+ });
+ 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       input: '',
+      imageURL: '',
     }
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({ input: event.target.value });
   }
 
   onButtonSubmit = () => {
-    console.log('click')
+    this.setState({imageURL: this.state.input});
+    app.models.initModel({id: Clarifai.FACE_DETECT_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
+      .then(generalModel => {
+        return generalModel.predict(this.state.input);
+      })
+      .then(response => {
+        var concepts = response['outputs'][0]['data']['concepts']
+      })
+ 
   }
 
   render() {
@@ -32,8 +47,7 @@ class App extends Component {
         onInputChange={this.onInputChange} 
         onButtonSubmit={this.onButtonSubmit}
         />
-        {/*
-        <FaceRecognition />*/}
+        <FaceRecognition imageURL={this.state.imageURL}/>
       </div>
     );
   }
